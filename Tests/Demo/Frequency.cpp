f541fa1e73777d06584b78bcbdd360f836e562c5
Jajauma <jajauma@2ch.hk>
2017-07-25 16:11:45 +0300

    Fix namespace after application types move

--- Tests/Demo/Frequency.cpp
+++ Tests/Demo/Frequency.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+using namespace Demo;
+
 TEST(Frequency, UsingCxx11Auto)
 {
     auto f = 1_Hz;
@@ -14,40 +16,40 @@ TEST(Frequency, UsingCxx11Auto)
 
 TEST(Frequency, Hz)
 {
-    SI::Frequency<int> f = 144_Hz;
+    Frequency<int> f = 144_Hz;
     EXPECT_EQ(f.Count(), 144);
-    SI::Frequency<double> g = 300000.0_Hz;
+    Frequency<double> g = 300000.0_Hz;
     EXPECT_FLOAT_EQ(g.Count(), 300000.0);
 }
 
 TEST(Frequency, kHz)
 {
-    SI::Frequency<int, std::kilo> f = 125_kHz;
+    Frequency<int, std::kilo> f = 125_kHz;
     EXPECT_EQ(f.Count(), 125);
-    SI::Frequency<double, std::kilo> g = 300.0_kHz;
+    Frequency<double, std::kilo> g = 300.0_kHz;
     EXPECT_FLOAT_EQ(g.Count(), 300.0);
 }
 
 TEST(Frequency, MHz)
 {
-    SI::Frequency<int, std::mega> f = -1_MHz;
+    Frequency<int, std::mega> f = -1_MHz;
     EXPECT_EQ(f.Count(), -1);
-    SI::Frequency<double, std::mega> g = 27.5_MHz;
+    Frequency<double, std::mega> g = 27.5_MHz;
     EXPECT_FLOAT_EQ(g.Count(), 27.5);
 }
 
 TEST(Frequency, GHz)
 {
-    SI::Frequency<int, std::giga> f = 0_GHz;
+    Frequency<int, std::giga> f = 0_GHz;
     EXPECT_EQ(f.Count(), 0);
-    SI::Frequency<double, std::giga> g = 3.3_GHz;
+    Frequency<double, std::giga> g = 3.3_GHz;
     EXPECT_FLOAT_EQ(g.Count(), 3.3);
 }
 
 TEST(Frequency, THz)
 {
-    SI::Frequency<int, std::tera> f = 430_THz;
+    Frequency<int, std::tera> f = 430_THz;
     EXPECT_EQ(f.Count(), 430);
-    SI::Frequency<double, std::tera> g = .11_THz;
+    Frequency<double, std::tera> g = .11_THz;
     EXPECT_FLOAT_EQ(g.Count(), 0.11);
 }
--- Tests/Demo/Length.cpp
+++ Tests/Demo/Length.cpp
@@ -2,50 +2,52 @@
 
 #include <gtest/gtest.h>
 
+using namespace Demo;
+
 TEST(Length, nm)
 {
-    SI::Length<int, std::nano> f = -99_nm;
+    Length<int, std::nano> f = -99_nm;
     EXPECT_EQ(f.Count(), -99);
-    SI::Length<float, std::nano> g = 10e9_nm;
+    Length<float, std::nano> g = 10e9_nm;
     EXPECT_FLOAT_EQ(g.Count(), 10e9);
 }
 
 TEST(Length, um)
 {
-    SI::Length<int, std::micro> f = 650_um;
+    Length<int, std::micro> f = 650_um;
     EXPECT_EQ(f.Count(), 650);
-    SI::Length<float, std::micro> g = 2.99_um;
+    Length<float, std::micro> g = 2.99_um;
     EXPECT_FLOAT_EQ(g.Count(), 2.99);
 }
 
 TEST(Length, mm)
 {
-    SI::Length<int, std::milli> f = 3_mm;
+    Length<int, std::milli> f = 3_mm;
     EXPECT_EQ(f.Count(), 3);
-    SI::Length<float, std::milli> g = 0.1_mm;
+    Length<float, std::milli> g = 0.1_mm;
     EXPECT_FLOAT_EQ(g.Count(), 0.1);
 }
 
 TEST(Length, cm)
 {
-    SI::Length<int, std::centi> f = 5_cm;
+    Length<int, std::centi> f = 5_cm;
     EXPECT_EQ(f.Count(), 5);
-    SI::Length<float, std::centi> g = 19_cm;
+    Length<float, std::centi> g = 19_cm;
     EXPECT_FLOAT_EQ(g.Count(), 19);
 }
 
 TEST(Length, m)
 {
-    SI::Length<int> f = 1_m;
+    Length<int> f = 1_m;
     EXPECT_EQ(f.Count(), 1);
-    SI::Length<float> g = 1.1_m;
+    Length<float> g = 1.1_m;
     EXPECT_FLOAT_EQ(g.Count(), 1.1);
 }
 
 TEST(Length, km)
 {
-    SI::Length<int, std::kilo> f = 72_km;
+    Length<int, std::kilo> f = 72_km;
     EXPECT_EQ(f.Count(), 72);
-    SI::Length<float, std::kilo> g = 40e3_km;
+    Length<float, std::kilo> g = 40e3_km;
     EXPECT_FLOAT_EQ(g.Count(), 40000.0);
 }
